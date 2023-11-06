0ab47717c1648573ea1d55853384cc1862e4855b
PrashantDas <78946070+PrashantDas@users.noreply.github.com>
2023-11-06 15:55:09 +0530

    Update settings.py

--- TweetsProject/settings.py
+++ TweetsProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3(!(#8khhl=fhv2=wymgsipt34y$wh)rdtu8mn21^_wrz245kk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['disastertweets.onrender.com']
 
 
 # Application definition
